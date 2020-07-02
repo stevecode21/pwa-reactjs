4a346b20df07fe1bc1ceeb024568cd8857911016
Steve Suarez <suarezsteve21@gmail.com>
2020-07-01 21:40:41 -0500

    Creating charging strategies (Cache first and network first) and implementing Google Analytics with offline support

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from "react-router-dom"
+// Cambiamos BrowserRouter por un router compun
+import { Router, Route, Link } from "react-router-dom"
 import Home from './pages/Home'
 import Recipe from './pages/Recipe'
 import Timer from './pages/Timer'
 import './App.css'
+// Importamos createBrowserHistory desde la librería history
+import { createBrowserHistory } from 'history'
+// Lo que haré será importar la librería de Google Analytics
+import ReactGA from 'react-ga'
 
+// Vamos a bindear esto a la historia de la aplicaciónen React Router
+// Creamos el plugin para la historia del browser, esto lo que nos permite hacer es trackear que páginas visitan los usuarios
+const history = createBrowserHistory()
+
+// Inicializamos REACT GA y usamos un id fake
+ReactGA.initialize('UA-000000-01')
+
+// Trackeamos la page view inicial cuando ingresamos a la aplicación
+ReactGA.pageview(window.location.pathname + window.location.search)
+
+// Agregamos este evento que nos va a decir cuando un usuario cambia de página
+history.listen(function (location) {
+  // Trackeamos una page view con la librería de GA y dentro del page view construimos la url que está visitando el usuario
+  ReactGA.pageview(window.location.pathname + window.location.search)
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      // Reemplazamos el BrowserRouter por Router y le agregamos el prop de history 
+      <Router history={history}>
         <div>
           <header>
             <Link to="/">Recetas</Link>
@@ -21,7 +42,7 @@ export default class App extends React.Component {
             <Route path="/timer" component={Timer} />
           </main>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
--- src/service-worker.js
+++ src/service-worker.js
@@ -7,6 +7,35 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 // registerNavigationRoute si tenemos algo ya cacheado en el SW que tiene algún tipo de comportamiento por defecto lo va a respetar, pero si tenemos una URL que no conocemos, pedirá a index.html que le diga a workbox lo que se debería mostrar
 workbox.routing.registerNavigationRoute('/index.html')
 
+// Con esto workbox va a capturar cada una de las request que hacemos de Google Analytics de nuestra aplicación, las va a guardar en memoria y cuando un usuario retome conexión será enviado correctametne a Google Analytics
+workbox.googleAnalytics.initialize()
+
+
+// Aplicaremos una regla nueva, agregamos un regexp para todas las request que se hacen a la API
+// Usaremos la estrategia StaleWhileRevalidate y solamente será para las petición de índole GET
+workbox.routing.registerRoute(/^https?:\/\/www.themealdb.com\/api\/.*/,
+  new workbox.strategies.StaleWhileRevalidate(), 'GET')
+
+// Registramos otra regla de Network First para las fonts que se traen de google apis, ahora ya que CacheFirst por ser la estrategia más peligrosas, le podemos poner un vencimiento al caché
+workbox.routing.registerRoute(/^https:\/\/fonts.(?:googleapis|gstatic).com\/(.*)/,
+  new workbox.strategies.CacheFirst(
+    {
+      // Para ponerle el vencimiento al cache, lo haremos dentro del método en un objeto, le añadimpos un nombre cualquiera al cache
+      cacheName: 'fonts-on-cache',
+      // Usamos un plugin de workbox, el que usaremos es el de expiration, que nos va a decir cuando vence este cache
+      // Ponemos una lista de plugins
+      plugins: [
+        // Creamos y llamamos el plugin
+        new workbox.expiration.Plugin({
+          // Entre las opciones del plugin, le otorgaremos un máximo de 30 días para su expiración
+          maxAgeSeconds: 86400
+        })
+      ]
+    }
+  ), 'GET')
+
+// Por defecto. Va al final de todo
 /* Por defecto las rutas que hagamos van a referir siempre al mismo dominio, si queremos setear que por defecto workbox utilice la estragia de Network First tenemos que especificar la expresión regular. Luego de eso, tenemos que especificarle qué estrategia queremos que utilice y por último podemos especificar qué metodos queremos que cachee (recomendable solo GET) */
 workbox.routing.registerRoute(/^https?.*/,
   new workbox.strategies.NetworkFirst(), 'GET')
+
