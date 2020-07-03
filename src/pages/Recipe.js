98ec0f3b5f620c809576e7f2c602cf3227bf4297
Steve Suarez <suarezsteve21@gmail.com>
2020-07-02 19:24:01 -0500

    Web share API

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -15,42 +15,73 @@ export default class Recipe extends React.Component {
     var recipe = null
     try {
       recipe = await mealdb.getRecipe(this.props.match.params.recipeId)
-    } catch(e) {
+    } catch (e) {
       recipe = null
     }
     this.setState({ recipe, isLoading: false })
   }
 
+  // Definimos un método para compartir, el cual toma un evento de click
+  compartir = (e) => {
+    // Impedimos la recarga del evento
+    e.preventDefault()
+
+    // Detectamos si el browser tiene la Web Share API, si no lo tiene, ponemos un alert
+    if (!navigator.share) {
+      alert("Tu browser no sporta la Web Share API");
+      return;
+    }
+
+    // Accedemos a la receta para compartir la receta
+    const { recipe } = this.state
+
+    // Llamamos a web share api
+    navigator.share({
+      // Mandamos un objeto, el title es el nombre del contenido que estamos compartiendo
+      title: `${recipe.name}`,
+      // El texto es la descripción del contenido que estamos compartiendo
+      text: 'Una receta deliciosa',
+      // Está será la url de la compartida. debería ser absoluta en lo posible
+      url: document.location.href
+    })
+      // Esto devuelve una promise con un método then que se va a ejecutar en caso de que todo esté bien
+      .then(() => alert("Contenido compartido!"))
+      // Catcheamos el error
+      .catch((error) => alert("Hubo un error"))
+  }
+
   render() {
     const { recipe, isLoading } = this.state
 
-    if( isLoading ) {
+    if (isLoading) {
       return <div className="message">Cargando...</div>
     }
-    else if( recipe === null ) {
+    else if (recipe === null) {
       return <div className="message">Hubo un problema :(</div>
     }
 
     return <div className="Recipe">
       <Helmet>
-        <title>{ recipe.name }</title>
+        <title>{recipe.name}</title>
       </Helmet>
 
       <div className="hero" style={{ backgroundImage: `url(${recipe.thumbnail})` }} />
-      
+
       <div className="title">
         <div className="info">
-          <h1>{ recipe.name }</h1>
-          <p>{ recipe.origin }</p>
+          <h1>{recipe.name}</h1>
+          <p>{recipe.origin}</p>
         </div>
         <div>
+          {/* Agregamos un link con el evento de compartir */}
+          <a onClick={this.compartir}>Compartir</a>
         </div>
       </div>
 
 
-      <RecipeIngredients ingredients={ recipe.ingredients } />
+      <RecipeIngredients ingredients={recipe.ingredients} />
 
-      <RecipeInstructions instructions={ recipe.instructions } />
+      <RecipeInstructions instructions={recipe.instructions} />
 
     </div>
   }
